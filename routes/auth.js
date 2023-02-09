17e91f6bb2a08f4dcf0fc6db1ebdc0448f84388a
Harsh Singh <sujeetsingh734321@gmail.com>
2023-02-09 14:38:03 +0530

    fgdfg

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", (req, res) => {
-
   var clientId = "d70aad72e6ca71e7d2114a38bfd4630a";
   var username = "techtreeapisandboxb2b@woohoo.in";
   var password = "techtreeapisandboxb2b@123";
@@ -17,18 +16,11 @@ router.get("/", (req, res) => {
   axios
     .post("https://sandbox.woohoo.in/oauth2/verify", data)
     .then((response) => {
-      const authorizationCode = response.data.authorizationCode;
-      if (authorizationCode) {
-        console.log(authorizationCode);
-        getToken(authorizationCode)
-          .then((bearerToken) => {
-            console.log(bearerToken);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
+      if (response.status === 200) {
+        console.log(response.data);
+        res.send(response.data);
       } else {
-        console.error("Error while getting authorization code");
+        console.error(response.data);
       }
     })
     .catch((err) => {
