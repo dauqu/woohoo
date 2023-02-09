30b862f6a57ab90964361dcbd8e745f1ec92898f
Harsh Singh <sujeetsingh734321@gmail.com>
2023-02-09 14:47:11 +0530

    fgjygh

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 4000;
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/auth", require("./routes/auth"));
-app.use("/token/:id", require("./routes/token"));
+//Get token by id 
+app.use("/token", require("./routes/token"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
