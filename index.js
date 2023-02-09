d6463a1c6e960dff6e97ca9e50d765d2d0c037df
Harsh Singh <sujeetsingh734321@gmail.com>
2023-02-09 14:18:03 +0530

    fgdfg

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = 4000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+app.use("/auth", require("./routes/auth"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/", (req, res) => {
+  //Axios post request to the auth server
+  $url = "https://sandbox.woohoo.in/oauth2/verify";
+
+  $clientId = "d70aad72e6ca71e7d2114a38bfd4630a";
+  $username = "techtreeapisandboxb2b@woohoo.in";
+  $password = "techtreeapisandboxb2b@123";
+
+  //Post request to the auth server
+  axios
+    .post($url, {
+      client_id: $clientId,
+      username: $username,
+      password: $password,
+    })
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
