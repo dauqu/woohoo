eb84675bbca6180c7af56d8b731918ce926f0337
Dauqu <sujeetsingh734321@gmail.com>
2023-02-17 20:41:09 +0530

    gdh

--- routes/categories.js
+++ routes/categories.js
@@ -1,20 +1,40 @@
 const router = require('express').Router();
 // default import axios 
-const {default: axios} = require('axios');
-const {token} = require('./constant')
+const { default: axios } = require('axios');
+const moment = require('moment');
+const crypto = require('crypto')
+
+
 
 router.get("/", (req, res) => {
-    try{
-        axios.get("https://sandbox.woohoo.in/rest/v3/catalog/categories/id", {
+    const { token } = req.query;
+
+    const method = 'GET';
+    const url = 'https://sandbox.woohoo.in/rest/v3/catalog/categories?q=1';
+
+    const baseString = `${method}&${url}`;
+
+    // url encode the base string
+    const baseStringEncoded = encodeURIComponent(baseString);
+
+    const signingKey = '6fd59feb7effbde5d3c1fba488db6e1a';
+    const signature = crypto.createHmac('sha256', signingKey)
+                     .update(baseStringEncoded)
+                     .digest('base64');
+
+    try {
+        axios.get("https://sandbox.woohoo.in/rest/v3/catalog/categories?q=1", {
             headers: {
-                "Authorization": `Bearer ${token}`
+                "Authorization": `Bearer ${token}`,
+                "dateAtClient": moment().toISOString(),
+                "signature": signature
             }
         }).then(data => {
             return res.json({
                 data: data.data
             })
         })
-    }catch(e){
+    } catch (e) {
         return res.json({
             message: e.message
         })
