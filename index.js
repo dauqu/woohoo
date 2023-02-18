0cc51c106e6055d7dd9a09fe8c36a99f81214c5c
Dauqu <sujeetsingh734321@gmail.com>
2023-02-18 12:43:49 +0530

    sg

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.use("/categories", require("./routes/categories"));
 //products
 app.use("/products", require("./routes/products"));
 
-app.listen(port, () => console.log(`Products added, Quick api listening on port ${port}!`));
+app.listen(port, () => console.log(`Products SKU, Quick api listening on port ${port}!`));
